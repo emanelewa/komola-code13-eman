ef186229dd63c90741a1d44561a1eafe965c8216
eman <eman.elewa@hotmail.com>
2018-05-17 20:30:03 +0200

    finished sunscripe & vedio

---
+++ js/js.js
@@ -0,0 +1,38 @@
+$(function(){
+    $("#video").videoPopup({
+        autoplay: 1,
+        controlsColor: 'white',
+        showVideoInformations: 0,
+        width: 1000,
+        customOptions: {
+            rel: 0,
+            end: 60
+        }
+    });
+    $("#video2").videoPopup();
+});
+
+
+var _gaq = _gaq || [];
+_gaq.push(['_setAccount', 'UA-36251023-1']);
+_gaq.push(['_setDomainName', 'jqueryscript.net']);
+_gaq.push(['_trackPageview']);
+
+(function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+})();
+
+/*slider*/
+$(function(){
+    SyntaxHighlighter.all();
+});
+$(window).load(function(){
+    $('.flexslider').flexslider({
+        animation: "slide",
+        start: function(slider){
+            $('body').removeClass('loading');
+        }
+    });
+});
